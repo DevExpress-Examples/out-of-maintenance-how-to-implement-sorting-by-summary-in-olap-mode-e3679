83c224dc9f2db7bbe15383d7a70a7f4608d25d38
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to implement Sorting by Summary in OLAP mode
+
+
+<p>The following example demonstrates how to implement Sorting by Summary in OLAP mode.</p><p>In this example, values of the Semester field are sorted by the Australia | Bendigo column summary values. To do this, two sort conditions represented by SortByCondition instances are created. One of them represents an OLAP member that corresponds to the 'Australia' value, while another corresponds to the 'Bendigo' value. These sort conditions are added to the Semester field's PivotGridField.SortByConditions collection to specify the column by which Semester values should be sorted. A data field that identifies the column is specified via the PivotGridField.SortByField property.</p><p>OLAP members corresponding to the Australia and Bendigo values are obtained using the PivotGridControl.GetFieldValueOlapMember method. Note that OLAP members can be obtained only for visible field values. For this reason, the Australia field value is expanded before initializing OLAP members in order to obtain the Bendigo OLAP member.</p><p>This sample uses the Adventure Works 2008 cube.<br />
+</p><br />
+
+
+<br/>
+
+
---
+++ VB/DXPivotGrid_OLAPSortByColumn/MainPage.xaml.vb
@@ -0,0 +1,55 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.PivotGrid
+
+Namespace DXPivotGrid_OLAPSortByColumn
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub pivotGridControl1_OlapDataLoaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+
+			' Expands the Australia column to be able to retrieve OLAP members 
+			' that correspond to the nested columns.
+            pivotGridControl1.ExpandValueAsync(True, _
+                                               New Object() {"Australia"}, _
+                                               Sub(res) SortByColumn(res))
+		End Sub
+		
+        Private Sub SortByColumn(ByVal res As Object)
+
+            ' Obtains OLAP members corresponding to the Australia and Bendigo values.
+            Dim countryMember As PivotOlapMember = _
+                pivotGridControl1.GetFieldValueOlapMember(fieldCountry, 0)
+            Dim cityMember As PivotOlapMember = _
+                pivotGridControl1.GetFieldValueOlapMember(fieldCity, 0)
+
+            ' Exits if the OLAP members were not obtained successfully.
+            If countryMember Is Nothing OrElse cityMember Is Nothing Then
+                Return
+            End If
+
+            ' Locks the pivot grid from updating while the Sort by Summary
+            ' settings are being customized.
+            pivotGridControl1.BeginUpdate()
+            Try
+
+                ' Specifies a data field whose summary values should be used to sort values
+                ' of the Month field.
+                fieldMonth.SortByField = fieldSales
+
+                ' Specifies a column by which the Month field values should be sorted.
+                fieldMonth.SortByConditions.Add( _
+                    New SortByCondition(fieldCountry, "Australia", countryMember.UniqueName))
+                fieldMonth.SortByConditions.Add( _
+                    New SortByCondition(fieldCity, "Bendigo", cityMember.UniqueName))
+            Finally
+
+                ' Unlocks the pivot grid and applies changes.
+                pivotGridControl1.EndUpdateAsync()
+            End Try
+        End Sub
+	End Class
+End Namespace
